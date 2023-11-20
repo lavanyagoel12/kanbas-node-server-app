5ce00db3e7a90a2c5471cf7df7459cc327818ca4
Lavanya <lavanya.goel12@gmail.com>
2023-11-20 12:54:33 -0500

    debugging

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import Hello from './hello.js';
 import Lab5 from "./lab5.js";
 import cors from "cors";
 import CourseRoutes from './courses/routes.js';
@@ -9,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+Hello(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
